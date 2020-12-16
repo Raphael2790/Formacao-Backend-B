be96d3d70938dc5dc421cf28e9c83cde7dc31b39
Raphael Silvestre dos Santos <raphael.silvestre@animaeducacao.com.br>
2020-12-16 09:08:24 -0300

    Adicionando autenticação na aplicação

---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace SHOP
+{
+    public class Settings
+    {
+        public static string Secret = "de47eb7ea2a5fd0f57f61444879f9dce";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,12 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using SHOP.Data;
 
 namespace SHOP
@@ -22,6 +25,28 @@ namespace SHOP
         {   
             //adicionando os controller ao services
             services.AddControllers();
+
+            //transformando a chave dentro de settings em bytes
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+            //adicionando autenticação ao services
+            services.AddAuthentication(x => 
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x => 
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true
+                };
+            } 
+            );
             //adicionando o contexto de banco de dados ao services
             //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
             services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
@@ -41,6 +66,9 @@ namespace SHOP
 
             app.UseRouting();
 
+            //Ordem muito importante autenticação e autorização
+            //Ambos são middlewares dentro da aplicação
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
