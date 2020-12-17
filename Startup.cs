2282cc63ae80329a6eff01a3e7e38e5178908372
Raphael Silvestre dos Santos <raphael.silvestre@animaeducacao.com.br>
2020-12-17 16:32:05 -0300

    Mudando página de erro para default

--- Startup.cs
+++ Startup.cs
@@ -76,8 +76,10 @@ namespace SHOP
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseHttpsRedirection();
 
